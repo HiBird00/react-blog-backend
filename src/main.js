16a0b80401495542c3063a2d0bf63ba473ec25ed
SuHyeon Kim <kim113355@naver.com>
2022-07-03 06:10:03 +0900

    jwt auth 구현

--- src/main.js
+++ src/main.js
@@ -5,17 +5,18 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const { PORT, MONGO_URI } = process.env;
 
-mongoose.connect(MONGO_URI)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch(e => {
-        console.error(e);
-    })
-
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const router = new Router();
 
@@ -25,10 +26,11 @@ router.use('/api', api.routes());
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+  console.log(`Listening to port ${port}`);
 });
